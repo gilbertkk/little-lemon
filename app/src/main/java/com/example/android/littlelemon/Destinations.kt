cf06fd1d8d06f071bf8f7e50943d927d03a6980b
Gilbert <gilbertkarim@gmail.com>
2025-05-19 13:01:52 +0100

    Initial navigation implemented

---
+++ app/src/main/java/com/example/android/littlelemon/Destinations.kt
@@ -0,0 +1,16 @@
+package com.example.android.littlelemon
+
+
+interface Destinations {
+    val route: String
+}
+
+object HomeDestination : Destinations {
+    override val route: String
+        get() = "Home"
+}
+
+object ProfileDestination: Destinations {
+    override val route: String
+        get() = "Profile"
+}
--- app/src/main/java/com/example/android/littlelemon/OnBoardingScreen.kt
+++ app/src/main/java/com/example/android/littlelemon/OnBoardingScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,7 +14,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,11 +25,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android.littlelemon.ui.theme.LittleLemonColor
 import com.example.android.littlelemon.ui.theme.LittleLemonTextStyle
-import com.example.android.littlelemon.ui.theme.LittleLemonTheme
 
 @Composable
 fun OnBoardingScreen(paddingValues: PaddingValues) {
@@ -97,19 +93,4 @@ fun OnBoardingScreen(paddingValues: PaddingValues) {
         }
 
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun OnBoardingScreenPreview() {
-    LittleLemonTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            topBar = { MyTopAppBar(true)},
-        ) { innerPadding ->
-            OnBoardingScreen(innerPadding
-            )
-        }
-    }
-
 }
